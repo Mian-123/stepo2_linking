b202935643131dd44d000c217aeed4b93a3884e3
Muhammad Waleed <mianwaleed2272@gmail.com>
2024-03-30 04:09:56 -0700

    ax

---
+++ src/app/name/Address/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+
+function Output3(){
+    return <div>Hello i am  from Address Page Lahore, Pakistan
+      <p><Link href="/name">Go to the name page page</Link></p>
+      
+    </div>
+    
+  }
+  export default Output3
---
+++ src/app/name/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link"
+
+function Output2(){
+    return <div>Hello i am Name page from Muhammad Waleed<br></br>
+     <p><Link href="/name/Address">Go to the Address page</Link></p>
+    </div>
+  }
+  export default Output2
